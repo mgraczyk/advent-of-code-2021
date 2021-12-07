76aaf74e5fddb8c09a30d8a0bc497ec26e0e1a6f
Michael Graczyk <mgraczyk@google.com>
2021-12-06 23:19:55 -0600

    day2 part 2

--- day/02/main.py
+++ day/02/main.py
@@ -1,6 +1,7 @@
 import os
 input_path = os.path.join(os.path.dirname(__file__), "input.txt")
 
+# part 1
 distance, depth = 0, 0
 
 for line in open(input_path):
@@ -12,5 +13,21 @@ for line in open(input_path):
     depth -= value
   elif direction == "down":
     depth += value
+
+print(depth * distance)
+
+# part 2
+distance, depth, aim = 0, 0, 0
+
+for line in open(input_path):
+  direction, value_str = line.strip().split()
+  value = int(value_str)
+  if direction == "forward":
+    distance += value
+    depth += aim * value
+  elif direction == "up":
+    aim -= value
+  elif direction == "down":
+    aim += value
 
 print(depth * distance)
