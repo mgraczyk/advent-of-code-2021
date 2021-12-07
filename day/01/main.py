ff389fde18c240d1651244ffc52dd630e8c1c6b7
Michael Graczyk <mgraczyk@google.com>
2021-12-06 23:15:22 -0600

    day1 part 2

--- day/01/main.py
+++ day/01/main.py
@@ -1,5 +1,16 @@
-values = [int(l) for l in open("input.txt")]
+import os
+input_path = os.path.join(os.path.dirname(__file__), "input.txt")
+
+values = [int(l) for l in open(input_path)]
 result = sum(1
     for prev, curr in zip(values, values[1:])
+    if curr > prev)
+print(result)
+
+tuples = zip(*(values[i:] for i in range(3)))
+tuple_sums = list(map(sum, tuples))
+
+result = sum(1
+    for prev, curr in zip(tuple_sums, tuple_sums[1:])
     if curr > prev)
 print(result)
